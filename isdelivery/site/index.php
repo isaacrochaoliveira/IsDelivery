a015d964af218404599be07bb766abe7d40728f4
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-12-02 23:31:46 -0300

    IsD

--- isdelivery/site/index.php
+++ isdelivery/site/index.php
@@ -50,24 +50,42 @@ require_once('../conf/config.php');
             </div>
             <div class="row menu">
                 <div class="col-md-6">
-                    <img src="<?= PATH ?>assets/imgs/jpgs/order-one.jpg" alt="Order Number One - Batata Fritas" width="250">
+                    <img src="<?= PATH ?>assets/imgs/jpgs/order-one.jpg" alt="Order Number One" width="250">
                     <div>
                         <h4 class="fs-22pt">Chips</h4>
                         <span class="fs-26pt color-yellow">R$22,99</span>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, iure debitis. Sint alias consequuntur deserunt magni eius id neque sunt labore excepturi quos iure incidunt, quidem blanditiis? Fuga, voluptatum quae.</p>
-                        <a href="#"><button type="button" class="btn-menu">I wanna order it</button></a>
+                        <a href="#"><button type="button" class="btn-menu">I Wanna Order</button></a>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    Olá
+                    <img src="<?= PATH ?>assets/imgs/jpgs/order-two.jpg" alt="Order Number Two" width="250">
+                    <div>
+                        <h4 class="fs-22pt">Coke with Burger</h4>
+                        <span class="fs-26pt color-yellow">R$16,00</span>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit omnis, iste, quas laboriosam tempora iure minima eum pariatur repellat, similique ex rem. A ea in nesciunt incidunt suscipit! Debitis, velit.</p>
+                        <a href=""><button type="button" class="btn-menu">I Wanna order</button></a>
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    Olá
+                    <img src="<?= PATH ?>assets/imgs/jpgs/order-three.jpg" alt="Order Number Three" width="250">
+                    <div>
+                        <h4 class="fs-22pt">Rice With Chicken</h4>
+                        <span class="fs-26pt color-yellow">R$32,50</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quia architecto ab voluptatum quam nesciunt nobis deserunt, quod veritatis consectetur cumque vitae officia nisi possimus magni commodi soluta ullam! Facere.</p>
+                        <a href=""><button type="button" class="btn-menu">I Wanna Order</button></a>
+                    </div>
                 </div>
                 <div class="col-md-6">
-                    Olá
+                    <img src="<?= PATH ?>assets/imgs/jpgs/order-fourth.jpg" alt="Order Number Fourth" width="250">
+                    <div>
+                        <h4 class="fs-22pt">MilkShake</h4>
+                        <span class="fs-26pt color-yellow">R$8,32</span>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa saepe fugit, mollitia magni temporibus architecto cupiditate dolores quae recusandae eos reprehenderit dolorem soluta libero ullam! Necessitatibus nobis earum nam aliquid!</p>
+                        <a href=""><button class="btn-menu" type="button">I Wanna Order</button></a>
+                    </div>
                 </div>
             </div>
         </section>
